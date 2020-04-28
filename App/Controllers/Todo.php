a837a2075cf085fc1c4967be80486ebc1f4d4266
borodatych <borodatych@demka.org>
2020-04-28 20:00:19 +0300

    bug fix #1

--- App/Controllers/Todo.php
+++ App/Controllers/Todo.php
@@ -23,7 +23,7 @@ class Todo extends Access
     {
         /// Подключаем скрипты для это страницы
         /// $this->arrCSS[] = '';
-        $this->arrJS[] = '/assets/js/pages/todo/datatable.js?2';
+        $this->arrJS[] = '/assets/js/pages/todo/datatable.js?3';
         $this->arrJS[] = '/assets/js/pages/todo/form.js?1';
 
         $this->template->content = $this->partial('Sections',[]);
